9b2cac88582f9f2d927401bde5b011c6b4c3fb7f
Shen Chen <shenchen@microsoft.com>
2023-05-17 14:02:44 -0700

    format

--- Artifacts/CutomerPostman/install-softwares.ps1
+++ Artifacts/CutomerPostman/install-softwares.ps1
@@ -7,44 +7,44 @@
 
 #
 
-# PowerShell configurations
+# PowerShell configurations
 
 #
 
 
 
 
-# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
 
-#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
 
-$ErrorActionPreference = "Stop"
-
-
-
-
-# Hide any progress bars, due to downloads and installs of remote components.
-
-$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "Stop"
 
 
 
 
-# Ensure we force use of TLS 1.2 for all downloads.
+# Hide any progress bars, due to downloads and installs of remote components.
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-
-
-
-# Allow certian operations, like downloading files, to execute.
-
-Set-ExecutionPolicy Bypass -Scope Process -Force
+$ProgressPreference = "SilentlyContinue"
 
 
 
 
-# Discard any collected errors from a previous execution.
+# Ensure we force use of TLS 1.2 for all downloads.
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+
+
+# Allow certian operations, like downloading files, to execute.
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+
+
+
+# Discard any collected errors from a previous execution.
 
 $Error.Clear()
 
@@ -55,7 +55,7 @@ $Error.Clear()
 
 #
 
-# Handle all errors in this script.
+# Handle all errors in this script.
 
 #
 
@@ -66,74 +66,74 @@ trap
 
 {
 
-    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
+    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
 
-    #       script, unless you want to ignore a specific error.
+    #       script, unless you want to ignore a specific error.
 
-    $message = $Error[0].Exception.Message
+    $message = $Error[0].Exception.Message
 
-    if ($message)
+    if ($message)
 
-    {
+    {
 
-        Write-Host -Object "`nERROR: $message" -ForegroundColor Red
+        Write-Host -Object "`nERROR: $message" -ForegroundColor Red
 
-    }
-
-
-
-
-    Write-Host "`nThe artifact failed to apply.`n"
+    }
 
 
 
 
-    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    Write-Host "`nThe artifact failed to apply.`n"
 
-    # returns exit code zero from the PowerShell script when using -File. The workaround is to
 
-    # NOT use -File when calling this script and leverage the try-catch-finally block and return
 
-    # a non-zero exit code from the catch block.
 
-    exit -1
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+
+    # a non-zero exit code from the catch block.
+
+    exit -1
 
 }
 
 
 
 
-function Ensure-PowerShell
+function Ensure-PowerShell
 
 {
 
-    [CmdletBinding()]
+    [CmdletBinding()]
 
-    param(
+    param(
 
-        [int] $Version
+        [int] $Version
 
-    )
-
-
+    )
 
 
-    if ($PSVersionTable.PSVersion.Major -lt $Version)
 
-    {
 
-        throw "The current version of PowerShell is $($PSVersionTable.PSVersion.Major). Prior to running this artifact, ensure you have PowerShell $Version or higher installed."
+    if ($PSVersionTable.PSVersion.Major -lt $Version)
 
-    }
+    {
+
+        throw "The current version of PowerShell is $($PSVersionTable.PSVersion.Major). Prior to running this artifact, ensure you have PowerShell $Version or higher installed."
+
+    }
 
 }
 
 
 
 
-Write-Host "start of ps1" + (Get-Date).DateTime
+Write-Host "start of ps1" + (Get-Date).DateTime
 
-Write-Output $PSVersionRequired
+Write-Output $PSVersionRequired
 
 
 
@@ -143,15 +143,15 @@ $env:computername
 
 
 
-Write-Host 'Configuring PowerShell session.'
+Write-Host 'Configuring PowerShell session.'
 
-Ensure-PowerShell -Version $PSVersionRequired
+Ensure-PowerShell -Version $PSVersionRequired
 
-Enable-PSRemoting -Force -SkipNetworkProfileCheck | Out-Null
+Enable-PSRemoting -Force -SkipNetworkProfileCheck | Out-Null
 
 hostname
 
-Write-Host "Preparing to install the Postman."
+Write-Host "Preparing to install the Postman."
 
 
 
@@ -160,55 +160,55 @@ try
 
 {
 
-    $PWD.Path 
+    $PWD.Path 
 
-    Write-Output $PSScriptRoot
-
-
-
-
-    Push-Location $PSScriptRoot
+    Write-Output $PSScriptRoot
 
 
 
 
-    
-
-    #Expand-Archive -LiteralPath sqldeveloper-22.2.1.234.1810-x64.zip -DestinationPath . 
+    Push-Location $PSScriptRoot
 
 
 
 
-    #Write-Host Unzipped sql developer successfully
+    
+
+    #Expand-Archive -LiteralPath sqldeveloper-22.2.1.234.1810-x64.zip -DestinationPath . 
 
 
 
 
-    Write-Host "Installing postmantest."
+    #Write-Host Unzipped sql developer successfully
 
-    #$p = Start-Process -Wait "Postman-win64-Setup.exe" -ArgumentList "/s" -PassThru
 
-    & .\Postman-win64-Setup.exe -s /123 /SP- /SUPPRESSMSGBOXES /VERYSILENT /NORESTART /MERGETASKS="!runcode" -n 900 >$NULL
 
-    #Start-Process -Wait .\sqldeveloper\sqldeveloper.exe --ArgumentList "/silent/debug/force/waitforcompletion" -PassThru
 
-    Write-Host "`nThe artifacts was applied successfully.`n"
+    Write-Host "Installing postmantest."
+
+    #$p = Start-Process -Wait "Postman-win64-Setup.exe" -ArgumentList "/s" -PassThru
+
+    & .\Postman-win64-Setup.exe -s /123 /SP- /SUPPRESSMSGBOXES /VERYSILENT /NORESTART /MERGETASKS="!runcode" -n 900 >$NULL
+
+    #Start-Process -Wait .\sqldeveloper\sqldeveloper.exe --ArgumentList "/silent/debug/force/waitforcompletion" -PassThru
+
+    Write-Host "`nThe artifacts was applied successfully.`n"
 
 }catch{
 
-    # Catch will pick up any non zero error code returned
+    # Catch will pick up any non zero error code returned
 
-    # You can do anything you like in this block to deal with the error, examples below:
+    # You can do anything you like in this block to deal with the error, examples below:
 
-    # $_ returns the error details
+    # $_ returns the error details
 
-    # This will just write the error
+    # This will just write the error
 
-    Write-Host "installation returned the following error $_"
+    Write-Host "installation returned the following error $_"
 
-    # If you want to pass the error upwards as a system error and abort your powershell script or function
+    # If you want to pass the error upwards as a system error and abort your powershell script or function
 
-    Throw "Aborted installation returned $_"
+    Throw "Aborted installation returned $_"
 
 }
 
@@ -216,12 +216,12 @@ finally
 
 {
 
-    Write-Host "CONSOLEEXITCODE : $LASTEXITCODE"
+    Write-Host "CONSOLEEXITCODE : $LASTEXITCODE"
 
-    #$p.Kill();
+    #$p.Kill();
 
-    Write-Host "end of ps1" + (Get-Date).DateTime
+    Write-Host "end of ps1" + (Get-Date).DateTime
 
-    Pop-Location
+    Pop-Location
 
 }
